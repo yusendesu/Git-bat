50e05dbf5916fc4f26302b71bb4f8d92c732b41b
yusendesu <yusendesu@outlook.com>
2025-10-10 23:12:34 +0800

    终于结束了

--- git_greenwall.py
+++ git_greenwall.py
@@ -4,13 +4,6 @@ import random
 
 
 def git_bulk_empty_commit(repo_path, days=365, min_commits=1, max_commits=3):
-    """
-    一次性提交多个带不同日期的空 commit
-    :param repo_path: Git 仓库路径
-    :param days: 模拟多少天的提交
-    :param min_commits: 每天最少 commit 数
-    :param max_commits: 每天最多 commit 数
-    """
     os.chdir(repo_path)
 
     for day in range(days):
